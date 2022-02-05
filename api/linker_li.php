623fb16590c563247bab6112117f24c3904c0469
Angel Balderas <angel.balderas@xpressindustrial.com>
2022-02-05 12:34:35 -0800

    /api/linker_li.php - Especificaron datos de conexión.

--- api/linker_li.php
+++ api/linker_li.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('US/Pacific');
 //$mysqli = mysqli_connect('localhost', 'usuario', 'password', 'nombre_base_de_datos');
-$mysqli = mysqli_connect('localhost', '', '', '');
+$mysqli = mysqli_connect('localhost', 'root', 'root', 'proyecto');
 
 if (!$mysqli) {
     printf("Can't connect to localhost. Error: %s\n", mysqli_connect_error());
