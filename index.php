0e4bd93949b20322e35e0cf57599b917ec7ed2d3
Angel Balderas <angel.balderas@xpressindustrial.com>
2022-02-12 10:26:43 -0800

    /index.php - Se agregó formulario y se indentó.

--- index.php
+++ index.php
@@ -11,99 +11,43 @@
   </head>
   <body>
     <div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-			<div class="jumbotron">
-				<h2>
-					Hello, world!
-				</h2>
-				<p>
-					This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.
-				</p>
-				<p>
-          <div class="row">
-            <div class="col-md-6">
-					         <a class="btn btn-primary btn-large" id="btnLearn" href="#">Learn more</a>
-            </div>
-            <div class="col-md-6">
-                  <a class="btn btn-danger btn-large" id="btnDanger" onclick="Reves();" href="#">Learn more</a>
-            </div>
-
+      <div class="row">
+        <div class="col-md-12">
+          <table id="tabla" class="table thead-light">
+            <thead>
+              <tr>
+                <th>
+                  Codigo
+                </th>
+                <th>
+                  Descripcion
+                </th>
+                <th>
+                  Precio
+                </th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <input type="text" id="codigo" class="form-control codigo" value="" />
+                </td>
+                <td>
+                  <input type="text" id="descripcion" class="form-control descripcion" value="" />
+                </td>
+                <td>
+                  <input type="number" id="precio" class="form-control precio" value="" />
+                </td>
+                <td>
+                  <button type="button" class="btn btn-success" id="btnAgregar" onclick="AgregarProducto();">Agregar</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-				</p>
-			</div>
-			<table class="table" id="tabla">
-				<thead>
-					<tr>
-						<th>
-							#
-						</th>
-						<th>
-							Product
-						</th>
-						<th>
-							Payment Taken
-						</th>
-						<th>
-							Status
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>
-							1
-						</td>
-						<td>
-							TB - Monthly
-						</td>
-						<td>
-							01/04/2012
-						</td>
-						<td>
-							Default
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-4">
-			<h2>
-				Heading
-			</h2>
-			<p>
-				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-			</p>
-			<p>
-				<a class="btn" href="#">View details »</a>
-			</p>
-		</div>
-		<div class="col-md-4">
-			<h2>
-				Heading
-			</h2>
-			<p>
-				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-			</p>
-			<p>
-				<a class="btn" href="#">View details »</a>
-			</p>
-		</div>
-		<div class="col-md-4">
-			<h2>
-				Heading
-			</h2>
-			<p>
-				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-			</p>
-			<p>
-				<a class="btn" href="#">View details »</a>
-			</p>
-		</div>
-	</div>
-</div>
+      </div>
+    </div>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -119,8 +63,10 @@ $(document).ready(function(){
   });
 });
 
-function Reves(){
-  $("#btnLearn").show();
-  $('#tabla').show();
+function AgregarProducto(){
+    var codigo = $('#codigo').val();
+    var descripcion = $('#descripcion').val();
+    var precio = $('#precio').val();
+    alert(codigo+' - '+descripcion+' - '+precio);
 }
 </script>
