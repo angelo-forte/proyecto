7cf7c653a9cbcfed5389512f9c4705ee89fe7187
Angel Balderas <angel.balderas@xpressindustrial.com>
2022-02-12 11:55:07 -0800

    /api/api.php - Se agregó codigo para guardar productos.

--- api/api.php
+++ api/api.php
@@ -1,13 +1,43 @@
 <?php
 require_once('linker_li.php');
 $mysqli->query("SET NAMES 'utf8'");
-$tipo = $_REQUEST['tipo'];
+$metodo = $_REQUEST['metodo'];
+
+if ($metodo == 'Pruebota'){
+  echo "Esta es la pruebota grandota";
+}
+
+if ($metodo == 'Pruebita'){
+  echo "Esta es la pruebita chiquita";
+}
+
+if ($metodo == 'GuardaProducto'){
+  $codigo = $_POST['codigo'];
+  $descripcion = $_POST['descripcion'];
+  $precio = $_POST['precio'];
+  $status = 0;
+
+  $query = "insert into productos (codigo, descripcion, precio)
+            values ('$codigo','$descripcion','$precio')";
+  $result = $mysqli->query($query);
+  if ($result != FALSE){
+    $status = 1;
+  }
+
+  if ($status == 1){
+    echo json_encode(array('status' => 'Success'));
+  }
+  else {
+    echo json_encode(array('status' => 'Error'));
+  }
+
+}
 
 /*
 if ($tipo == ''){
 	$id = $_REQUEST['id'];
 	$status = 0;
-	$query = "select id, razon_social,codigo 
+	$query = "select id, razon_social,codigo
 	          from clientes where id=500";
 	$result = $mysqli->query($query);
 	if ($result != FALSE) {
